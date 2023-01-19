ea096656264fccf4df25635e1fb0afc7b089a93d
Jayanga L Pathiraja <Lakmal.P.Jayanga@gds.ey.com>
2023-01-19 12:03:31 +0530

    done some corrections

--- src/com/sample/process/ProcessNumbers.java
+++ src/com/sample/process/ProcessNumbers.java
@@ -7,59 +7,163 @@ import java.util.Scanner;
 
 public class ProcessNumbers {
     public static void main(String[] args) throws IOException {
-        System.out.println("Please Enter a Number :");
+        processNumbers();
+    }
+
+    private static void processNumbers() throws IOException {
+        System.out.println("please enter your input:");
         Scanner scanner = new Scanner(System.in);
 
-        //Reading first input parameter
-        String number = scanner.nextLine();
-        int numb = Integer.parseInt(number);
+        String input = scanner.nextLine();
+        String[] arr = input.split(" ");
+//        System.out.println(input.substring(12));
+//        System.out.println(arr.length);
 
-        List<Integer> list = readNumbersFromFile("src/com/sample/process/input_numbers.txt");
+        if(arr.length==2){
+            String second = arr[1];
+           processNumb(second);
 
-        System.out.println("Should Result be  written to a file ? ( y / n )");
-        //Reading Second input parameter
-        String charactor = scanner.nextLine();
 
-        if(numb%2 == 0 && charactor.equals("y")){
-            List<Integer> evenList = new ArrayList<>();
-            for (Integer i:list) {
-                if (i%2==0){
-                    evenList.add(i);
+        }else if(arr.length==3){
+            String second = arr[1];
+            String third = arr[2];
+            ProcessNumbs(second,third);
+        }
+    }
+
+    private static void processNumb(String second) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        if(isInteger(second)){
+            System.out.println("Please enter list of numbers(space separated integers) :");
+            String nextInput = scanner.nextLine();
+//            int number = Integer.parseInt(second);
+            String[] strNumb = nextInput.split(" ");
+            int number = strNumb.length;
+
+            if(number%2==0){
+                List<Integer> evenList = new ArrayList<>();
+                for (String s:strNumb) {
+                    int num = Integer.parseInt(s);
+                    if(num%2==0){
+                        evenList.add(num);
+                    }
                 }
-
-            }
-            writeNumbersToFile(evenList);
-        }else if(numb%2 == 0){
-            for (Integer i:list) {
-                if (i%2==0){
+                for (Integer i:evenList) {
                     System.out.println(i);
                 }
+            }else{
+                List<Integer> oddList = new ArrayList<>();
+                for (String s:strNumb) {
+                    int num = Integer.parseInt(s);
+                    if(num%2==1){
+                        oddList.add(num);
+                    }
+                }
+                for (Integer i:oddList) {
+                    System.out.println(i);
+                }
+            }
+        }else{
+            if(second.substring(0,5).equalsIgnoreCase("input")){
+                List<Integer> list = readNumbersFromFile("src/com/sample/process/input.txt");
+
+                if(list.size()%2==0){
+                    List<Integer> evenList = new ArrayList<>();
+                    for (Integer i:list) {
+
+                        if(i%2==0){
+                            evenList.add(i);
+                        }
+                    }
+                    for (Integer x:evenList) {
+                        System.out.println(x);
+                    }
+                }else{
+                    List<Integer> oddList = new ArrayList<>();
+                    for (Integer i:list) {
+
+                        if(i%2==1){
+                            oddList.add(i);
+                        }
+                    }
+                    for (Integer x:oddList) {
+                        System.out.println(x);
+                    }
+                }
+
 
             }
-        }
-        else{
-            if(charactor.equals("y")){
-                List<Integer> oddList = new ArrayList<>();
-                for (Integer i:list) {
-                    if (i%2==1){
-                        //writeNumber(i);
-                        oddList.add(i);
-                    }
 
+
+        }
+    }
+
+    private static void ProcessNumbs(String second,String third) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        if(isInteger(second)){
+            System.out.println("Please enter list of numbers(space separated integers) :");
+            String nextInput = scanner.nextLine();
+//            int number = Integer.parseInt(second);
+            String[] strNumb = nextInput.split(" ");
+            int number = strNumb.length;
+
+            if(number%2==0){
+                List<Integer> evenList = new ArrayList<>();
+                for (String s:strNumb) {
+                    int num = Integer.parseInt(s);
+                    if(num%2==0){
+                        evenList.add(num);
+                    }
+                }
+                writeNumbersToFile(evenList);
+            }else{
+                List<Integer> oddList = new ArrayList<>();
+                for (String s:strNumb) {
+                    int num = Integer.parseInt(s);
+                    if(num%2==1){
+                        oddList.add(num);
+                    }
                 }
                 writeNumbersToFile(oddList);
-            }else {
-                for (Integer i:list) {
-                    if (i%2==1){
-                        System.out.println(i);
-                    }
+            }
+        }else{
+            if(second.substring(0,5).equalsIgnoreCase("input")){
+                List<Integer> list = readNumbersFromFile("src/com/sample/process/input.txt");
 
+                if(list.size()%2==0){
+                    List<Integer> evenList = new ArrayList<>();
+                    for (Integer i:list) {
+
+                        if(i%2==0){
+                            evenList.add(i);
+                        }
+                    }
+                    writeNumbersToFile(evenList);
+                }else{
+                    List<Integer> oddList = new ArrayList<>();
+                    for (Integer i:list) {
+
+                        if(i%2==1){
+                            oddList.add(i);
+                        }
+                    }
+                    writeNumbersToFile(oddList);
                 }
+
+
             }
 
+
         }
+    }
 
-
+    private static boolean isInteger(String str){
+        try {
+            Integer.parseInt(str);
+            return true;
+        }catch (NumberFormatException ex){
+            return false;
+        }
     }
 
     private static List<Integer> readNumbersFromFile(String filename) throws IOException {
@@ -94,7 +198,7 @@ public class ProcessNumbers {
         BufferedWriter bw = null;
         try {
 
-            File file = new File("src/com/sample/process/output_numbers.txt");
+            File file = new File("src/com/sample/process/output.txt");
 
             if (!file.exists()) {
                 file.createNewFile();
